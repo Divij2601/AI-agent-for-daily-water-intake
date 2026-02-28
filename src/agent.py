75dbdff959e95e29b10354add31a44e53fb7b9fe
Divij2601 <divij26dudeja@gmail.com>
2026-02-28 21:54:45 +0530

    Add WaterIntakeAgent class to analyze hydration based on water intake; integrate ChatGoogleGenerativeAI for feedback generation.

--- src/agent.py
+++ src/agent.py
@@ -5,4 +5,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-load
+GOOGLE_API_KEY= os.getenv("GOOGLE_API_KEY")
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5,
+)
+
+class WaterIntakeAgent:
+    
+    def __init__(self):
+        self.history = []
+
+    def analyze_intake(self, intake_ml):
+
+        prompt= f"""
+        You are a hydration assistant. The user has consumed {intake_ml} ml of water today. Proide a hydration status and check if they need to drink more water.
+        """
+        response= llm.invoke([HumanMessage(content=prompt)])
+
+        return response.content
+
+if __name__== "__main__":
+    agent=WaterIntakeAgent()
+    intake=1500
+    feedback = agent.analyze_intake(intake)
+    print(f"Hydration Analysis: {feedback}")
+
+
+
